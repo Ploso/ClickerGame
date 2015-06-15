ced7aa64b33ed0bfcd4fb32c6cbb2ee47d937d81
Ploso <eero.leppaniemi@gmail.com>
2015-06-15 12:37:49 +0300

    ENSICOMMIT!

---
+++ Assets/Scripts/GameGlobals.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameGlobals : MonoBehaviour {
+
+	public static int rahe = 0;
+	
+
+	public static int GetRahe()
+	{
+		return rahe;
+	}
+
+	public static void SetRahe(int tempRahe)
+	{
+		rahe = tempRahe;
+	}
+}
---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUDScript : MonoBehaviour {
+
+	public Text HUDRahe;
+	private string screenRahe;
+
+	public Button raheButton;
+	private int rahe;
+
+
+	void Start () {
+		raheButton = raheButton.GetComponent<Button>();
+	}
+
+	public void RaheGains()
+	{
+		rahe = GameGlobals.GetRahe();
+		rahe++;
+		GameGlobals.SetRahe (rahe);
+	}
+
+	void Update () {
+		screenRahe = GameGlobals.rahe.ToString();
+		HUDRahe.text = "Rahe: " + screenRahe;
+
+	}
+}
