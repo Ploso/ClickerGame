669053fd39698728259f1829b51c82ccb74e9b0c
Ploso <eero.leppaniemi@gmail.com>
2015-06-15 13:48:09 +0300

    Klikki1
    Kikki hiiri

--- Assets/Scripts/GameGlobals.cs
+++ Assets/Scripts/GameGlobals.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class GameGlobals : MonoBehaviour {
 
 	public static int rahe = 0;
+	public static int ayyyluminium = 0;
+
+	public static float autoRahe = 1f;
+	public static int clickRahe = 1;
 	
 
 	public static int GetRahe()
@@ -15,4 +19,37 @@ public class GameGlobals : MonoBehaviour {
 	{
 		rahe = tempRahe;
 	}
+
+	public static float GetAuto()
+	{
+		return autoRahe;
+	}
+	
+	public static void SetAuto(float tempAuto)
+	{
+		autoRahe = tempAuto;
+	}
+
+	public static int GetClick()
+	{
+		return clickRahe;
+	}
+
+	public static void SetClick(int tempClick)
+	{
+		clickRahe = tempClick;
+	}
+
+	public static int GetAyy()
+	{
+		return ayyyluminium;
+	}
+	
+	public static void SetAyy(int tempAyy)
+	{
+		ayyyluminium = tempAyy;
+	}
+
+
+
 }
--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -5,26 +5,94 @@ using UnityEngine.UI;
 public class HUDScript : MonoBehaviour {
 
 	public Text HUDRahe;
+	public Text HUDAyy;
 	private string screenRahe;
+	private string screenAyy;
 
+	public Button autoRahe1;
+	public Button increaseAuto;
 	public Button raheButton;
+	public Button clickIncrease;
+	public Button buyAyy;
+
 	private int rahe;
 
-
+	
 	void Start () {
 		raheButton = raheButton.GetComponent<Button>();
+		autoRahe1 = autoRahe1.GetComponent<Button>();
+		increaseAuto = increaseAuto.GetComponent<Button> ();
+		clickIncrease = clickIncrease.GetComponent<Button> ();
+		buyAyy = buyAyy.GetComponent<Button> ();
+
 	}
 
-	public void RaheGains()
-	{
-		rahe = GameGlobals.GetRahe();
-		rahe++;
-		GameGlobals.SetRahe (rahe);
-	}
 
 	void Update () {
 		screenRahe = GameGlobals.rahe.ToString();
 		HUDRahe.text = "Rahe: " + screenRahe;
 
+		screenAyy = GameGlobals.ayyyluminium.ToString();
+		HUDAyy.text = "Ayyyluminium: " + screenAyy;
+
+	}
+
+	void AutoRahe()
+	{
+		rahe = GameGlobals.GetRahe();
+		rahe++;
+		GameGlobals.SetRahe (rahe);
+		Invoke ("AutoRahe", GameGlobals.autoRahe);
+	}
+
+	public void SetAutoRahe()
+	{
+		int tempRahe;
+		if (GameGlobals.rahe >= 20) {
+			tempRahe = GameGlobals.GetRahe();
+			tempRahe = tempRahe -20;
+			GameGlobals.SetRahe(tempRahe);
+			AutoRahe ();
+		}
+	}
+
+	public void RaheGains()
+	{
+		int click;
+		click = GameGlobals.GetClick ();
+		rahe = GameGlobals.GetRahe();
+		rahe = rahe + click;
+		GameGlobals.SetRahe (rahe);
+	}
+
+	public void RaheAutoIncrease()
+	{
+		float speed;
+		speed = GameGlobals.GetAuto();
+		speed = speed/1.1f;
+		GameGlobals.SetAuto (speed);
+	}
+
+	public void ClickIncrease()
+	{
+		int click;
+		click = GameGlobals.GetClick();
+		click++;
+		GameGlobals.SetClick (click);
+	}
+
+	public void BuyAyy()
+	{
+		int ayy;
+		int tempRahe;
+
+		if (GameGlobals.rahe >= 100) {
+			ayy = GameGlobals.GetAyy ();
+			ayy++;
+			GameGlobals.SetAyy (ayy);
+			tempRahe = GameGlobals.GetRahe();
+			tempRahe = tempRahe -100;
+			GameGlobals.SetRahe(tempRahe);
+		}
 	}
 }
